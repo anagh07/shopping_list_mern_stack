0af68286ed5a0aae86b6f8204ce0e058c7dbe3c6
Anagh Mehran <anagh07@gmail.com>
2020-03-15 03:22:35 -0400

    Implemented list view & add button

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './App.css';
-import './components/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AppNavBar from './components/AppNavBar.js';
+
+import './App.css';
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <AppNavBar />
+      <ShoppingList />
     </div>
   );
 }
